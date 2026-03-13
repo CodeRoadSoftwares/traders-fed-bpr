f9a781c11e1a8ba4883f0142768716d4e8973f66
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:14:03 +0530

    FUND CATEGORIES

---
+++ constants/fundCategories.ts
@@ -0,0 +1,16 @@
+export const INCOME_CATEGORIES = [
+  "membership_fees",
+  "donations",
+  "event_revenue",
+  "sponsorship",
+  "other_income",
+] as const;
+
+export const EXPENSE_CATEGORIES = [
+  "events",
+  "relief_fund",
+  "administrative",
+  "infrastructure",
+  "legal",
+  "other_expense",
+] as const;
