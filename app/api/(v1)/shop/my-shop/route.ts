5e19eb2b6ad57cf94fa4f0225e6a88f2ea900737
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:07:49 +0530

    MYSHOP API

---
+++ app/api/(v1)/shop/my-shop/route.ts
@@ -0,0 +1,24 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import Shop from "@/models/shop.model";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectDb();
+    const user = await getUser();
+    if (!user) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const shop = await Shop.findOne({
+      userId: new Types.ObjectId(user.id),
+    }).populate("userId", "-password");
+    if (!shop) {
+      return NextResponse.json({ message: "shop not found" }, { status: 404 });
+    }
+    return NextResponse.json(shop, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
