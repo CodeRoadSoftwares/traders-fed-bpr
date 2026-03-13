ec71b562d6080068eab0925dc25c71b06687959d
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:12:50 +0530

    UPDATE SHOP API

---
+++ app/api/(v1)/shop/update/route.ts
@@ -0,0 +1,37 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import Shop from "@/models/shop.model";
+import { shopSchema } from "@/validation/shop/shop.validation";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function PUT(req: Request) {
+  try {
+    await connectDb();
+    const user = await getUser();
+    if (!user) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const data = await req.json();
+    const { id, ...updateData } = data;
+    const parsedData = shopSchema.parse(updateData);
+    const shop = await Shop.findById(new Types.ObjectId(id));
+    if (!shop) {
+      return NextResponse.json({ message: "shop not found" }, { status: 404 });
+    }
+    if (shop.userId.toString() !== user.id && user.role === "SHOP") {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const updated = await Shop.findByIdAndUpdate(
+      new Types.ObjectId(id),
+      parsedData,
+      { new: true },
+    );
+    return NextResponse.json(
+      { message: "shop updated successfully", data: updated },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
