88f02573f091762150f8e2aca48c39853fd87f52
bhatbazik <bhatbazik@gmail.com>
2026-03-13 10:43:44 +0530

    get admin api

---
+++ app/api/(v1)/admin/get/route.ts
@@ -0,0 +1,39 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDb();
+    const user = await getUser();
+    const isAdmin = user?.role === "ADMIN" || user?.role === "SUPER_ADMIN";
+    if (!isAdmin) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const page = Number(req.nextUrl.searchParams.get("page")) || 1;
+    const limit = Number(req.nextUrl.searchParams.get("limit")) || 10;
+    const skip = (page - 1) * limit;
+
+    const [data, total] = await Promise.all([
+      User.find({ role: { $in: ["ADMIN", "SUPER_ADMIN"] } })
+        .select("-password")
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit),
+      User.countDocuments({ role: { $in: ["ADMIN", "SUPER_ADMIN"] } }),
+    ]);
+
+    return NextResponse.json({
+      data,
+      pagination: {
+        total,
+        page,
+        limit,
+        pages: Math.ceil(total / limit),
+      },
+    });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
