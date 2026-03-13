37b647e50d61cc60aadd4ca3490c1ab2f301fed6
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:00:35 +0530

    CREATE NOTICE API

---
+++ app/api/(v1)/notice/create/route.ts
@@ -0,0 +1,35 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import Notice from "@/models/notice.model";
+import { INotice, noticeSchema } from "@/validation/notice/notice.validation";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    await connectDb();
+    const user = await getUser();
+    const isAdmin = user?.role === "ADMIN" || user?.role === "SUPER_ADMIN";
+    if (!isAdmin) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const data = await req.json();
+    const parsedData: INotice = noticeSchema.parse(data);
+    const notice = await Notice.create({
+      ...parsedData,
+      createdBy: new Types.ObjectId(user.id),
+    });
+    if (!notice) {
+      return NextResponse.json(
+        { message: "failed to create notice" },
+        { status: 400 },
+      );
+    }
+    return NextResponse.json(
+      { message: "notice created successfully", data: notice },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
