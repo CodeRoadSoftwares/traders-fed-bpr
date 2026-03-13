54ee31025d8de4319d942aef4757b392aa8bb10a
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:06:33 +0530

    DELETE SHOP API

---
+++ app/api/(v1)/shop/delete/route.ts
@@ -0,0 +1,27 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import Shop from "@/models/shop.model";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req: Request) {
+  try {
+    await connectDb();
+    const user = await getUser();
+    const isAdmin = user?.role === "ADMIN" || user?.role === "SUPER_ADMIN";
+    if (!isAdmin) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const { id } = await req.json();
+    const deleted = await Shop.findByIdAndDelete(new Types.ObjectId(id));
+    if (!deleted) {
+      return NextResponse.json({ message: "shop not found" }, { status: 404 });
+    }
+    return NextResponse.json(
+      { message: "shop deleted successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
