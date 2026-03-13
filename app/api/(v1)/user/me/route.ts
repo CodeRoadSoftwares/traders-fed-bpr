282ec95728c86b158dac3e66c5e4930f9cc865fc
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:13:25 +0530

    USER ME AND USER UPDATE API

---
+++ app/api/(v1)/user/me/route.ts
@@ -0,0 +1,24 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import User from "@/models/user.model";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectDb();
+    const user = await getUser();
+    if (!user) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const userData = await User.findById(new Types.ObjectId(user.id)).select(
+      "-password",
+    );
+    if (!userData) {
+      return NextResponse.json({ message: "user not found" }, { status: 404 });
+    }
+    return NextResponse.json(userData, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
---
+++ app/api/(v1)/user/update/route.ts
@@ -0,0 +1,39 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import User from "@/models/user.model";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+export async function PUT(req: Request) {
+  try {
+    await connectDb();
+    const user = await getUser();
+    if (!user) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const data = await req.json();
+    const { name, phone, address, password } = data;
+    const updateData: Record<string, unknown> = {};
+    if (name) updateData.name = name;
+    if (phone) updateData.phone = phone;
+    if (address) updateData.address = address;
+    if (password) {
+      updateData.password = await hash(password, 10);
+    }
+    const updated = await User.findByIdAndUpdate(
+      new Types.ObjectId(user.id),
+      updateData,
+      { new: true },
+    ).select("-password");
+    if (!updated) {
+      return NextResponse.json({ message: "user not found" }, { status: 404 });
+    }
+    return NextResponse.json(
+      { message: "profile updated successfully", data: updated },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
