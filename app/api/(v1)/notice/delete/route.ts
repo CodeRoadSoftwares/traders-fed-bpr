9e8287ae3d3f78bd0f49826c3ff4d3357a9b8d8b
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:01:09 +0530

    DELETE NOTICE API

---
+++ app/api/(v1)/notice/delete/route.ts
@@ -0,0 +1,30 @@
+import { getUser } from "@/lib/auth/getUser";
+import { connectDb } from "@/lib/db/db";
+import Notice from "@/models/notice.model";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req: Request) {
+  try {
+    await connectDb();
+    const user = await getUser();
+    const isAdmin = user?.role === "ADMIN" || user?.role === "SUPER_ADMIN";
+    if (!isAdmin) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+    const { id } = await req.json();
+    const deleted = await Notice.findByIdAndDelete(new Types.ObjectId(id));
+    if (!deleted) {
+      return NextResponse.json(
+        { message: "notice not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(
+      { message: "notice deleted successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
