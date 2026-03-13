f3f72a37620978bcabdf906c936eae14f0e871fb
bhatbazik <bhatbazik@gmail.com>
2026-03-13 11:02:56 +0530

    GET NOTICE BY ID API

---
+++ app/api/(v1)/notice/get/[id]/route.ts
@@ -0,0 +1,33 @@
+import { connectDb } from "@/lib/db/db";
+import Notice from "@/models/notice.model";
+import { Types } from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  _req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  try {
+    await connectDb();
+    const { id } = params;
+    if (!Types.ObjectId.isValid(id)) {
+      return NextResponse.json(
+        { message: "Invalid notice id" },
+        { status: 400 },
+      );
+    }
+    const notice = await Notice.findById(new Types.ObjectId(id)).populate(
+      "createdBy",
+      "name",
+    );
+    if (!notice) {
+      return NextResponse.json(
+        { message: "notice not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(notice, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
