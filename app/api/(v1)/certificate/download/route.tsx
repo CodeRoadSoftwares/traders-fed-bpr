17c33660ffad1682c3f7852eaaf179bea6705684
bhatbazik <bhatbazik@gmail.com>
2026-04-03 10:23:26 +0530

    fix build

--- app/api/(v1)/certificate/download/route.tsx
+++ app/api/(v1)/certificate/download/route.tsx
@@ -311,7 +311,7 @@ export async function GET(req: NextRequest) {
       />,
     );
 
-    return new NextResponse(pdfBuffer, {
+    return new NextResponse(new Uint8Array(pdfBuffer), {
       status: 200,
       headers: {
         "Content-Type": "application/pdf",
